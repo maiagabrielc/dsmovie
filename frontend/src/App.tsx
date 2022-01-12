959a42de69fe190ab43c6a48dcfc5e46f7696571
Gabriel Maia <maiagabrielc@gmail.com>
2022-01-12 19:48:24 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,21 @@
-import Navbar from '../src/components/Navbar/index';
-
+import { BrowserRouter,
+  Route,
+  Routes } from 'react-router-dom';
+  import Listing from 'pages/Listing';
+  import Navbar from 'components/Navbar/index';
+  import Form from 'pages/Form';
+  
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,6 @@
+export default function Form() {
+
+  return (
+    <h1>Página de formulário</h1>
+  );
+}
